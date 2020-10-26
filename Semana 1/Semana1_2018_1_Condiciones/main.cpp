8b13274fb53e7e90c83a531c863708c9a5aa9510
Elitrea <elitrejo0219@gmail.com>
2020-10-26 15:40:41 -0600

    Semana1_Condiciones

--- Semana 1/Semana1_2018_1_Condiciones/main.cpp
+++ Semana 1/Semana1_2018_1_Condiciones/main.cpp
@@ -4,36 +4,36 @@ using namespace std;
 
 int main()
 {
-    float salario;
+    float salario=0;
     cin>>salario;
 
     if(salario>=0.01&&salario<=969.5)
     {
-        cout<<((salario-0.01)*(0.192));
+        cout<<((salario-0.01)*(0.0192))+0;
     }
-    else if(salario>=969.51&&salario<=1703.8)
+    if(salario>=969.51&&salario<=1703.8)
     {
         cout<<((salario-969.51)*(0.1088))+56.91;
     }
-    else if(salario>=1703.81&&salario<=4872.61)
+   if(salario>=1703.81&&salario<=4872.61)
     {
         cout<<((salario-1703.81)*(0.16))+136.85;
     }
-    else if(salario>=4782.62&&salario<=14391.44)
+    if(salario>=4782.62&&salario<=14391.44)
     {
         cout<<((salario-4782.62)*(0.2352))+766.15;
     }
-    else if(salario>=14391.45&&salario<=19188.61)
+   if(salario>=14391.45&&salario<=19188.61)
     {
         cout<<((salario-14391.45)*(0.32))+3470.25;
     }
-    else if(salario>=19188.62&&salario<=57656.76)
+    if(salario>=19188.62&&salario<=57565.76)
     {
         cout<<((salario-19188.62)*(0.34))+5005.35;
     }
-    else if(salario>=57565.77)
+    if (salario>=57565.77&&salario<=200000)
     {
-        cout<<((salario-57656.77)*(0.35))+18053.63;
+        cout<<((salario-57565.77)*(0.35))+18053.63;
     }
     return 0;
 }
--- Semana 1/Semana1_Marcos de puertas/main.cpp
+++ Semana 1/Semana1_Marcos de puertas/main.cpp
@@ -6,6 +6,23 @@ int main()
 {
 int n,a,b,res;
 cin>>n>>a>>b;
-
+res=((a*4)+(b*2))/n;
+if((((a*4)+(b*2))%n)>0)
+    {
+       res=res+1;
+    }
+if (a*4<n)
+    {
+     res=res+1;
+    }
+if(b*2<n)
+    {
+       res=res+1;
+    }
+else
+{
+cout<<res;
+}
+cout<<res;
     return 0;
 }
