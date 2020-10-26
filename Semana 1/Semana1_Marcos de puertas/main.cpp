fb8ee4807f5f54502a79d0560802bf8f577b016a
Elitrea <elitrejo0219@gmail.com>
2020-10-26 16:07:12 -0600

    avance

--- Semana 1/Semana1_Marcos de puertas/main.cpp
+++ Semana 1/Semana1_Marcos de puertas/main.cpp
@@ -7,18 +7,19 @@ int main()
 int n,a,b,res;
 cin>>n>>a>>b;
 res=((a*4)+(b*2))/n;
-if((((a*4)+(b*2))%n)>0)
-    {
-       res=res+1;
-    }
-if (a*4<n)
+if(a*2>n&&b*2>n)
+       {
+     res=res+2;
+        }
+else if(a*2+b*2>n)
+       {
+     res=res+1;
+        }
+else if (a*2>n||a*4>n||b*2>n)
     {
      res=res+1;
     }
-if(b*2<n)
-    {
-       res=res+1;
-    }
-cout<<res;
+
+    cout<<res;
     return 0;
 }
